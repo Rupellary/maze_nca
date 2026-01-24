53fe13097e5f94a37e8f158e64c8cde62c9cd702
Rockwell <rockwellgulassa@gmail.com>
2026-01-24 18:22:39 +0100

    fix: training loop now working, although the reward function needs work

--- src/maze_nca/train.py
+++ src/maze_nca/train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from typing import Optional, Callable
 from maze_nca.nca import NCAModel
 from maze_nca.config import EnvConfig
+from maze_nca.reward import softmax_reward
 from tensorflow.keras.optimizers import AdamW
 
 @tf.function
@@ -9,7 +10,8 @@ def train_step(
     nca: NCAModel,
     task: tf.Tensor,
     optimizer,
-    config: EnvConfig
+    config: EnvConfig,
+    seed: tf.constant,
 ) -> tf.Tensor:
     """
     Performs one rollout of NCA and applies backpropogation.
@@ -21,7 +23,7 @@ def train_step(
     task : tf.Tensor
         Tensor with batch of mazes' non-living channels, shape (B, H, W, non_living)
     optimizer : AdamW
-        optimizer for backprop
+        Optimizer for backprop
     config : EnvConfig
         Config object specifying simulation parameters
         Attributes used: [
@@ -29,33 +31,36 @@ def train_step(
             rollout_steps,
             loss_gamma
         ]
+    seed : tf.constant <--- FINISH THIS
 
     Returns:
     ----------
     total_loss : tf.Tensor
-        Total loss from the rollout
+        Total cumulative loss from the rollout
     """
 
-    compute_reward: Callable[[tf.Tensor], tf.Tensor] = config.reward_function
+    compute_reward = softmax_reward # <--- TEMPORARY REWARD FUNCTION ASSIGNMENT
 
-    x: tf.Tensor = nca.egg(task)
-    r: tf.Tensor = compute_reward(x)
-    T: int = config.rollout_steps
-    discount: tf.constant = tf.constant(1.0)
+    # Add living channels to task tensor
+    world: tf.Tensor = nca.egg(task)
+
+    # Intiliaze reward, discount, and cumulative loss
+    previous_reward: tf.Tensor = compute_reward(world, config)
+    discount: tf.constant = tf.constant(1.0) # discount will strengthen over rollout
     total_loss: tf.constant = tf.constant(0.0)
 
     with tf.GradientTape() as tape:
 
-        # x = nca.embryogenesis(x)
+        # world = nca.embryogenesis(world)
 
-        for _ in range(T):
+        for _ in range(config.rollout_steps):
             # Update state
-            x = nca(x)
+            world = nca(world, seed)
             # Compute state reward
-            rt = compute_reward(x)
+            current_reward = compute_reward(world, config)
             # Compute negative delta reward (value will be minimized)
-            neg_delta_r = r - rt
-            r = rt
+            neg_delta_r = previous_reward - current_reward
+            previous_reward = current_reward
             # Apply discount
             loss = neg_delta_r * discount
             discount *= config.loss_gamma
@@ -70,7 +75,9 @@ def train_step(
 def train_loop(
     nca: NCAModel,
     task: tf.Tensor,
-    config: EnvConfig
+    optimizer,
+    config: EnvConfig,
+    seed: tf.constant,
 ) -> list[float]:
     """
     Performs one rollout of NCA and applys backpropogation.
@@ -79,12 +86,15 @@ def train_loop(
     ----------
     nca : NCAModel
         Model to train
+    task : tf.Tensor                    <--- FINISH THIS
+    optimizer :                         <--- FINISH THIS
     config : EnvConfig
         Config object specifying simulation parameters
         Attributes used: [
             lr, weight_decay,
             epochs
         ]
+    seed : tf.constant                  <--- FINISH THIS
 
     Returns:
     ----------
@@ -92,18 +102,14 @@ def train_loop(
         List containing the loss after each epoch
     """
 
-    optimizer = AdamW(
-        learning_rate=config.lr,
-        weight_decay=config.weight_decay
-    )
-
-    history = []
+    history: list[float] = []
 
     for i in range(config.epochs):
 
-        loss = train_step(nca, task, optimizer, config)
+        loss: float = train_step(nca, task, optimizer, config, seed)
         history.append(loss)
 
+        # Print loss every 10 epochs
         if (i + 1) % 10 == 0:
             print(loss)
 
